1f77c2ca6e7a924b26c6c035ff5afc448cc3c935
William <williamsimiao@gmail.com>
2018-09-06 15:58:39 -0300

    trying to read file

--- Makefile
+++ Makefile
@@ -1,11 +1,8 @@
 CFLAGS = -O
 CC = gcc
 
-projeto1:projeto1.o readgml.o
-	$(CC) $(CFLAGS) -o projeto1 projeto1.o readgml.o
+projeto1:projeto1.o 
+	$(CC) $(CFLAGS) -o projeto1 projeto1.o
 
-projeto1.o:projeto1.c readgml.h network.h Makefile
+projeto1.o:projeto1.c  Makefile
 	$(CC) $(CFLAGS) -c projeto1.c
-
-readgml.o:readgml.c network.h Makefile
-	$(CC) $(CFLAGS) -c readgml.c
--- projeto1.c
+++ projeto1.c
@@ -1,27 +1,54 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "readgml.h"
+#include <string.h>
 #include "network.h"
 
-void read_file(FILE *my_file) {
+void read_line(FILE *my_file) {
     char line[1000];
+    fscanf(my_file, "%[^\n]", line);
+    fgetc(my_file);
+        printf("z");
+
+}
+void read_chars(FILE * my_file, int rept) {
+    int i = 0;
+    while(i < rept) {
+        fgetc(my_file);
+    }
+}
+
+void read_file(FILE *my_file) {
     int i;
     my_file = fopen("karate.gml", "r");
     if(my_file == NULL){
         printf("Nao achei o arquivo\n");
         exit(1);
     }
-
+    // eliminando 4 linhas
     for(i=0; i < 4; i++) {
-        fscanf(my_file, "%[^\n]", line);
+        read_line(my_file);
     }
-    printf("%s\n", line);
+        printf("d");
 
+    int id;
+    read_line(my_file);
+    // Eh 7 pq sim
+    printf("a");
+    read_chars(my_file, 7);
+        printf("v");
 
+    fscanf(my_file, "%d", &id);
+    printf("o id eh: %d", id);
+    // if(strcmp(line, "node")){
+        
+
+    // }
 }
 
 int main() {
     FILE *my_file;
+    printf("s");
+
     read_file(my_file);
     return 0; 
 }
