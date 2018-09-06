9aee5470747f5d0be7230b53fb8ec0bf40ddfa73
William <williamsimiao@gmail.com>
2018-09-06 12:55:42 -0300

    makefile update

--- projeto1.c
+++ projeto1.c
@@ -1,7 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "readgml.h"
+#include "network.h"
 
 int main() {
+    printf("Hello2\n");
+    FILE *my_file = fopen("r", "karate/karate.gml");
+    printf("Hello3\n");
+    if(my_file != NULL){
+        printf("Nao achei o arquivo\n");
+    }
+    NETWORK *my_net;
+    read_network(my_net, my_file);
     printf("Hello\n");
     return 0; 
 }
